b6fc5ab86a76aa26657ad4de5e46d8b76ec0cba4
Nicolas Massé <nicolas.masse@itix.fr>
2025-05-27 18:35:09 +0200

    improve integration tests

--- test/load.sh
+++ test/load.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
+set -Eeuo pipefail
+
 # Replace these variables with your MQTT broker details
-MQTT_BROKER="localhost"
-MQTT_PORT=1883
-MQTT_TOPIC="images"
-MQTT_RESPONSE_TOPIC="results"
+MQTT_BROKER="${MQTT_BROKER:-localhost}"
+MQTT_PORT="${MQTT_PORT:-1883}"
+MQTT_TOPIC="${MQTT_TOPIC:-train-image}"
+MQTT_RESPONSE_TOPIC="${MQTT_RESPONSE_TOPIC:-train-model-result}"
 
 # Path to the image file
-IMAGE_FILE="../traffic-signs.jpg"
+IMAGE_FILE="${IMAGE_FILE:-test.jpg}"
 
-# Encode the image file as base64
-IMAGE_BASE64=$(base64 -w 0 "$IMAGE_FILE")
+declare -a float_list=()
 
-float_list=()
-
-for ((i = 1; i <= 3; i++)); do
+for ((i = 1; i <= 100; i++)); do
     echo "Iteration $i"
-    mosquitto_pub -h "$MQTT_BROKER" -p "$MQTT_PORT" -t "$MQTT_TOPIC" -m "$IMAGE_BASE64"
-    start=$(date +%s%N)
-    stop=$(mosquitto_sub -v -t results -C 1 | xargs -d$'\n' -L1 bash -c 'date +%s%N')
-    duration_ns=$((stop - start))
-    duration_ms=$((duration_ns / 1000000))
-    float_list+=("$duration_ms")
-    echo $duration_ms
-    sleep 1
+    start=$(date +%s.%N)
+    . publish.sh
+    stop=$(mosquitto_sub -v -t "$MQTT_RESPONSE_TOPIC" -C 1 | xargs -d$'\n' -L1 bash -c 'date +%s.%N')
+    duration=$(echo "$stop - $start" | bc -q /dev/stdin )
+    float_list+=("$duration")
+    echo $duration
+    sleep 0.1
 done
 
 calculate_mean() {
@@ -36,4 +34,4 @@ calculate_mean() {
 }
 
 mean=$(calculate_mean "${float_list[@]}")
-echo "Mean: $mean"
+echo "Mean: $mean s"
